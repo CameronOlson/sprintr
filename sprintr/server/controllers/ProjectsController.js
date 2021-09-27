75ee925d793a60d67e36b79b44aa9a3cb150c2c0
Cameron Olson <olsoncam10@gmail.com>
2021-09-27 16:31:18 -0600

    Project on server side

--- sprintr/server/controllers/ProjectsController.js
+++ sprintr/server/controllers/ProjectsController.js
@@ -1,3 +1,4 @@
+import { Auth0Provider } from '@bcwdev/auth0provider'
 import { projectsService } from '../services/ProjectsService.js'
 import BaseController from '../utils/BaseController.js'
 
@@ -6,12 +7,54 @@ export class ProjectsController extends BaseController {
     super('api/projects')
     this.router
       .get('', this.getProjects)
+      .get('/:projectId', this.getProjectById)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.createProject)
+      .put('/:projectId', this.editProject)
+      .delete('/:projectId', this.removeProject)
+  }
+
+  async removeProject(req, res, next) {
+    try {
+      const project = await projectsService.removeProject(req.params.projectId, req.userInfo.id)
+      res.send(project)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createProject(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      const project = await projectsService.createProject(req.body)
+      res.send(project)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getProjectById(req, res, next) {
+    try {
+      const project = await projectsService.getProjectById(req.params.projectId)
+      res.send(project)
+    } catch (error) {
+      next(error)
+    }
   }
 
   async getProjects(req, res, next) {
     try {
       const projects = await projectsService.getProjects(req.query)
       res.send(projects)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editProject(req, res, next) {
+    try {
+      const project = await projectsService.editProject(req.params.projectId, req.userInfo.id, req.body)
+      res.send(project)
     } catch (error) {
       next(error)
     }
--- sprintr/server/services/ProjectsService.js
+++ sprintr/server/services/ProjectsService.js
@@ -1,7 +1,43 @@
 import { dbContext } from '../db/DbContext'
-import { BadRequest } from '../utils/Errors'
+import { BadRequest, Forbidden } from '../utils/Errors'
 
 class ProjectsService {
+  async removeProject(projectId, userId) {
+    const project = await this.getProjectById(projectId)
+    if (userId !== project.creatorId.toString()) {
+      throw new Forbidden("you aren't allowed to do that!!")
+    }
+    await project.delete()
+    return project
+  }
+
+  async editProject(projectId, userId, projectData) {
+    const project = await this.getProjectById(projectId)
+    if (userId !== project.creatorId.toString()) {
+      throw new Forbidden("you aren't allowed to do that!")
+    }
+    project.name = projectData.name || project.name
+    project.description = projectData.description || project.description
+    await project.save()
+    return project
+  }
+
+  async createProject(projectData) {
+    const project = await dbContext.Projects.create(projectData)
+    if (!project) {
+      throw new BadRequest('there is no project ')
+    }
+    return project
+  }
+
+  async getProjectById(projectId) {
+    const project = await dbContext.Projects.findById(projectId)
+    if (!project) {
+      throw new BadRequest('this is not the right project ID')
+    }
+    return project
+  }
+
   async getProjects(query = {}) {
     const projects = await dbContext.Projects.find(query).populate('creator', 'name')
     if (!projects) {
