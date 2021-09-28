ef764e0cf6f74e310a2bbd22f49a3008bf220b6e
Cameron Olson <olsoncam10@gmail.com>
2021-09-27 22:18:21 -0600

    BacklogItemIdDataBacklogItemId

--- sprintr/server/controllers/BacklogItemsController.js
+++ sprintr/server/controllers/BacklogItemsController.js
@@ -1,3 +1,4 @@
+import { Auth0Provider } from '@bcwdev/auth0provider'
 import { backlogItemsService } from '../services/BacklogItemsService.js'
 import BaseController from '../utils/BaseController.js'
 
@@ -6,6 +7,49 @@ export class BacklogItemsController extends BaseController {
     super('api/projects/:projectId/backlogItems')
     this.router
       .get('', this.getBacklogItems)
+      .get('/:backlogItemId', this.getBacklogItemById)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.createBacklogItem)
+      .put('/:backlogItemId', this.editBacklogItem)
+      .delete('/:backlogItemId', this.removeBacklogItem)
+  }
+
+  async removeBacklogItem(req, res, next) {
+    try {
+      const backlogItem = await backlogItemsService.removeBacklogItem(req.params.backlogItemId, req.userInfo.id)
+      res.send(backlogItem)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editBacklogItem(req, res, next) {
+    try {
+      const backlogItem = await backlogItemsService.editBacklogItem(req.params.backlogItemId, req.userInfo.id, req.body)
+      res.send(backlogItem)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createBacklogItem(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      req.body.projectId = req.params.projectId
+      const backlogItem = await backlogItemsService.createBacklogItem(req.body)
+      res.send(backlogItem)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getBacklogItemById(req, res, next) {
+    try {
+      const backlogItem = await backlogItemsService.getBacklogItemById(req.params.backlogItemId)
+      res.send(backlogItem)
+    } catch (error) {
+      next(error)
+    }
   }
 
   async getBacklogItems(req, res, next) {
--- sprintr/server/services/BacklogItemsService.js
+++ sprintr/server/services/BacklogItemsService.js
@@ -1,13 +1,47 @@
 import { dbContext } from '../db/DbContext'
-import { BadRequest } from '../utils/Errors'
+import { BadRequest, Forbidden } from '../utils/Errors'
 
 class BacklogItemsService {
+  async removeBacklogItem(backlogItemId, userId) {
+    const backlogItem = await this.getBacklogItemById(backlogItemId)
+    if (userId !== backlogItem.creatorId.toString()) {
+      throw new Forbidden('You do not have that authority')
+    }
+    await backlogItem.delete()
+    return backlogItem
+  }
+
+  async editBacklogItem(backlogItemId, userId, backlogItemData) {
+    const backlogItem = await this.getBacklogItemById(backlogItemId)
+    if (userId !== backlogItem.creatorId.toString()) {
+      throw new Forbidden("you can't dp that")
+    }
+    backlogItem.name = backlogItemData.name || backlogItem.name
+    backlogItem.description = backlogItemData.description || backlogItem.description
+    backlogItem.status = backlogItemData.status || backlogItem.status
+    await backlogItem.save()
+    return backlogItem
+  }
+
+  async getBacklogItemById(backlogItemId) {
+    const backlogItem = await dbContext.BacklogItems.findById(backlogItemId)
+    if (!backlogItem) {
+      throw new BadRequest('no backlog item found by that ID')
+    }
+    return backlogItem
+  }
+
   async getBacklogItems() {
     const backlogItems = await dbContext.BacklogItems.find()
     if (!backlogItems) {
       throw new BadRequest('no matching BacklogItems')
     }
     return backlogItems
+  }
+
+  async createBacklogItem(backlogItemData) {
+    const backlogItem = await dbContext.BacklogItems.create(backlogItemData)
+    return backlogItem
   }
 }
 export const backlogItemsService = new BacklogItemsService()
