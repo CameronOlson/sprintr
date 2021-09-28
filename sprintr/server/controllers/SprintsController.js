52ce029ea182eaeab4eebe9c8f60dcf284e44b36
Jeremy-Fowler <jembyfowler@gmail.com>
2021-09-27 20:47:06 -0600

    finished sprints backend

--- sprintr/server/controllers/SprintsController.js
+++ sprintr/server/controllers/SprintsController.js
@@ -7,7 +7,49 @@ export class SprintsController extends BaseController {
     super('api/projects/:projectId/sprints')
     this.router
       .get('', this.getSprints)
+      .get('/:sprintId', this.getSprintById)
       .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.createSprint)
+      .put('/:sprintId', this.editSprint)
+      .delete('/:sprintId', this.removeSprint)
+  }
+
+  async removeSprint(req, res, next) {
+    try {
+      const sprint = await sprintsService.removeSprint(req.params.sprintId, req.userInfo.id)
+      res.send(sprint)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editSprint(req, res, next) {
+    try {
+      const sprint = await sprintsService.editSprint(req.params.sprintId, req.userInfo.id, req.body)
+      res.send(sprint)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createSprint(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      req.body.projectId = req.params.projectId
+      const sprint = await sprintsService.createSprint(req.body)
+      res.send(sprint)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getSprintById(req, res, next) {
+    try {
+      const sprint = await sprintsService.getSprintById(req.params.sprintId)
+      res.send(sprint)
+    } catch (error) {
+      next(error)
+    }
   }
 
   async getSprints(req, res, next) {
--- sprintr/server/services/SprintsService.js
+++ sprintr/server/services/SprintsService.js
@@ -1,6 +1,41 @@
 import { dbContext } from '../db/DbContext'
+import { BadRequest, Forbidden } from '../utils/Errors'
 
 class SprintsService {
+  async removeSprint(sprintId, userId) {
+    const sprint = await this.getSprintById(sprintId)
+    if (userId !== sprint.creatorId.toString()) {
+      throw new Forbidden('this is not your sprint!!')
+    }
+    await sprint.delete()
+    return sprint
+  }
+
+  async editSprint(sprintId, userId, sprintData) {
+    const sprint = await this.getSprintById(sprintId)
+    if (userId !== sprint.creatorId.toString()) {
+      throw new Forbidden('this is not your sprint!!')
+    }
+    sprint.name = sprintData.name || sprint.name
+    sprint.startDate = sprintData.startDate || sprint.startDate
+    sprint.endDate = sprintData.endDate || sprint.endDate
+    await sprint.save()
+    return sprint
+  }
+
+  async createSprint(sprintData) {
+    const sprint = await dbContext.Sprints.create(sprintData)
+    return sprint
+  }
+
+  async getSprintById(sprintId) {
+    const sprint = await dbContext.Sprints.findById(sprintId)
+    if (!sprint) {
+      throw new BadRequest('no sprint found')
+    }
+    return sprint
+  }
+
   async getSprints() {
     const sprints = await dbContext.Sprints.find()
     return sprints
