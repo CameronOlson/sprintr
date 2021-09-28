757b012ce01dda2cdf64cd1f4f08517ac57d8634
Jeremy Fowler <jembyfowler@gmail.com>
2021-09-28 14:24:44 -0600

    yeah

--- sprintr/server/services/NotesService.js
+++ sprintr/server/services/NotesService.js
@@ -31,7 +31,7 @@ class NotesService {
 
   async createNote(noteData) {
     const note = await dbContext.Notes.create(noteData)
-    await note.populate('creator', 'name picture').execPopulate()
+    await note.populate('creator', 'name picture')
     return note
   }
 
