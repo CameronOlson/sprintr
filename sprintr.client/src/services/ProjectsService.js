f9e6f3ec70676fa96e9062da2adcbe61e6ef1fce
Cameron Olson <olsoncam10@gmail.com>
2021-09-28 16:48:04 -0600

    project servise something

--- sprintr.client/src/services/ProjectsService.js
+++ sprintr.client/src/services/ProjectsService.js
@@ -1,3 +1,5 @@
+import { AppState } from '../AppState'
+import { Project } from '../models/Project'
 import { logger } from '../utils/Logger'
 import { api } from './AxiosService'
 
@@ -5,6 +7,13 @@ class ProjectsService {
   async createProject(newProject) {
     const res = await api.post('api/projects', newProject)
     logger.log('res from project creation', res)
+  }
+
+  async getProjects() {
+    const res = await api.get('api/projects')
+    logger.log(res)
+    AppState.projects = res.data.map(p => new Project(p))
+    logger.log(AppState.projects)
   }
 }
 export const projectsService = new ProjectsService()
