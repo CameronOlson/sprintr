d756306b412ca5484e75b288f6a84528f5a9b78b
Cameron Olson <olsoncam10@gmail.com>
2021-09-30 12:23:56 -0600

    delete tasks

--- sprintr.client/src/components/Task.vue
+++ sprintr.client/src/components/Task.vue
@@ -1,10 +1,15 @@
 <template>
   <li v-if="task.backlogItemId === backlogItem.id">
-    {{ task.name }} Weight: {{ task.weight }}
+    {{ task.name }} Weight: {{ task.weight }} <i v-if="account.id === task.creatorId" @click="removeTask()" class="mdi mdi-delete selectable"></i>
   </li>
 </template>
 
 <script>
+import { computed } from '@vue/runtime-core'
+import { AppState } from '../AppState'
+import { tasksService } from '../services/TasksService'
+import { useRoute } from 'vue-router'
+import Pop from '../utils/Pop'
 export default {
   props: {
     task: {
@@ -16,8 +21,20 @@ export default {
       required: true
     }
   },
-  setup() {
-    return {}
+  setup(props) {
+    const route = useRoute()
+    return {
+      account: computed(() => AppState.account),
+      async removeTask() {
+        try {
+          if (await Pop.confirm) {
+            await tasksService.removeTask(route.params.id, props.task.id)
+          }
+        } catch (error) {
+          Pop.toast(error, 'error')
+        }
+      }
+    }
   }
 }
 </script>
